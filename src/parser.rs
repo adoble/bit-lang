89924e6b390caac2ecfbf39cbde16f1c12965ff7
Andrew Doble <andrew.l.doble@gmail.com>
2024-02-23 13:59:53 +0100

    Basic hex

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,13 @@
-use nom::bytes::complete::{tag, take};
-use nom::number::complete::be_u16;
-use nom::IResult;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, one_of},
+    combinator::recognize,
+    multi::{many0, many1},
+    sequence::{preceded, terminated},
+    IResult, Parser,
+};
+// use nom::number::complete::be_u16;
 
 pub fn hex_header(input: &str) -> IResult<&str, &str> {
     tag("0x")(input)
@@ -10,6 +17,18 @@ pub fn hex_header(input: &str) -> IResult<&str, &str> {
 //     let (input, length) = be_u16(input)?;
 //     take(length)(input)
 // }
+
+fn hexadecimal(input: &str) -> IResult<&str, &str> {
+    // <'a, E: ParseError<&'a str>>
+    preceded(
+        alt((tag("0x"), tag("0X"))),
+        recognize(many1(terminated(
+            one_of("0123456789abcdefABCDEF"),
+            many0(char('_')),
+        ))),
+    )
+    .parse(input)
+}
 
 #[cfg(test)]
 mod tests {
@@ -21,5 +40,13 @@ mod tests {
         let (remaining, header) = hex_header(data).unwrap();
 
         assert_eq!(header, "0x");
+    }
+
+    #[test]
+    fn test_hexadecimal() {
+        let data = "0x45B7";
+        let (_, hex) = hexadecimal(data).unwrap();
+
+        assert_eq!(hex, "45B7");
     }
 }
