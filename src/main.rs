679e3e416e82e7b6decf4ae799ad2a0898da657d
Andrew Doble <andrew.l.doble@gmail.com>
2024-02-24 17:25:53 +0100

    Parse range using u8 parser

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::f32::consts::E;
-
-use crate::parser::*;
-
-mod parser;
-
-fn main() {
-    println!("Finished");
-}
--- src/parser.rs
+++ src/parser.rs
@@ -1,23 +1,27 @@
+#[allow(unused_imports)]
 use nom::{
     branch::alt,
     bytes::complete::{is_a, tag, take_while1},
-    character::complete::{char, one_of},
+    character::complete::{char, one_of, u8},
     character::is_digit,
-    combinator::{map, recognize},
+    combinator::{into, map, recognize, value},
     multi::{many0, many1},
-    sequence::{preceded, terminated, tuple},
-    IResult, Parser,
+    //number::complete::{i32, u8},
+    sequence::{preceded, separated_pair, terminated, tuple},
+    IResult,
+    Parser,
 };
-enum BitRange {
-    Single(usize),
-    Range(usize, usize),
-}
-fn index(input: &str) -> IResult<&str, &str> {
-    is_a("0123456789")(input)
-}
+// enum BitRange {
+//     Single(u8),
+//     Range(u8, u8),
+// }
+// fn index(input: &str) -> IResult<&str, u8> {
+//     (u8)(input)
+// }
 
-fn range(input: &str) -> IResult<&str, (&str, &str, &str)> {
-    tuple((index, tag(".."), index))(input)
+fn range(input: &str) -> IResult<&str, (u8, u8)> {
+    //tuple((index, tag(".."), index))(input)
+    separated_pair(u8, tag(".."), u8)(input)
 }
 
 // fn bits(input: &str) -> IResult<&str, &str> {
@@ -50,27 +54,27 @@ mod tests {
     fn test_range() {
         let data = "2..45";
         let (_, r) = range(data).unwrap();
-        assert_eq!(r, ("2", "..", "45"));
+        assert_eq!(r, (2, 45));
     }
 
-    #[test]
-    fn test_index() {
-        let data = "34";
-        let (_, i) = index(data).unwrap();
-        assert_eq!(i, "34");
+    // #[test]
+    // fn test_index() {
+    //     let data = "34";
+    //     let (_, i) = index(data).unwrap();
+    //     assert_eq!(i, 34);
 
-        let data = "7";
-        let (_, i) = index(data).unwrap();
-        assert_eq!(i, "7");
+    //     let data = "7";
+    //     let (_, i) = index(data).unwrap();
+    //     assert_eq!(i, 7);
 
-        let data = "48";
-        let (_, i) = index(data).unwrap();
-        assert_eq!(i, "48");
+    //     let data = "48";
+    //     let (_, i) = index(data).unwrap();
+    //     assert_eq!(i, 48);
 
-        let data = "2345;";
-        let (_, i) = index(data).unwrap();
-        assert_eq!(i, "2345");
-    }
+    //     let data = "2345;";
+    //     let (_, i) = index(data).unwrap();
+    //     assert_eq!(i, 2345);
+    // }
 
     #[test]
     fn test_hexadecimal() {
